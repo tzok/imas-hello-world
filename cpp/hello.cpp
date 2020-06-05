2e96b83310d131f5d620908e71be1983e550a4fb
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2020-06-05 11:31:25 +0200

    Add Hello World! in C++

---
+++ cpp/hello.cpp
@@ -0,0 +1,15 @@
+#include <UALClasses.h>
+
+int main() {
+    IdsNs::IDS ids(1, 1, 0, 0);
+    ids.createEnv("imas", "test", "3");
+
+    ids._summary.ids_properties.comment = "Hello World from C++";
+    ids._summary.ids_properties.homogeneous_time = 1;
+    ids._summary.time.resize(1);
+    ids._summary.time(0) = 0.1;
+    ids._summary.put();
+
+    ids.close();
+    return 0;
+}
